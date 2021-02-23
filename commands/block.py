ea99f0907a9dee10428205fd967562532278a9cb
fredi_68 <jascha.hirsekorn@googlemail.com>
2021-02-23 12:29:34 +0100

    merged block and unblock; implemented new ORM

--- commands/block.py
+++ commands/block.py
@@ -3,7 +3,9 @@
 import discord
 from cmdsys import *
 
-class MyCommand(Command):
+from core_models import BlockedUser
+
+class BlockUser(Command):
 
     def setup(self):
         
@@ -23,13 +25,42 @@ class MyCommand(Command):
             await self.respond("You can't block the bot owner!", True)
             return
 
-        db = self.db.getDatabaseByMessage(self.msg)
+        db = self.db.get_db_by_message(self.msg)
         
-        ds = db.createDatasetIfNotExists("blockedUsers", {"userID": member.id})
-        if ds.exists():
+        if len(db.query(BlockedUser).filter(user_id=member.id)) > 0:
             await self.respond("This user is already blocked.", True)
             return
 
-        ds.update() #block user
+        m = db.new(BlockedUser)
+        m.user_id = member.id
+        m.save()
         await self.respond("Successfully blocked user " + member.name + " (" + str(member.id) + ")", True)
+        return
+
+class Unblock(Command):
+
+    def setup(self):
+        
+        self.name = "unblock"
+        self.desc = "Unblocks a user from using bot commands."
+        self.addArgument(MemberArgument("member"))
+        self.ownerOnly = True
+        self.hidden = True
+
+    async def call(self, member, **kwargs):
+
+        if not isinstance(member, discord.Member):
+            await self.respond("Not a valid member.")
+            return
+
+        db = self.db.get_db_by_message(self.msg)
+        
+        ds = db.createDatasetIfNotExists("blockedUsers", {"userID": member.id})
+        q = db.query(BlockedUser).filter(user_id=member.id)
+        if len(q) < 1:
+            await self.respond("This user can't be unblocked since he was never blocked in the first place.", True)
+            return
+
+        q.delete() #unblock user
+        await self.respond("Successfully unblocked user "+member.name+" ("+member.id+")",True)
         return
